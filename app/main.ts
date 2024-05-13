33010b43deb17d816e1bfb2ca8a646b5ff577660
steven huynh <steven.h@live.com>
2024-05-12 22:20:24 -0700

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,6 @@
+import * as fs from 'fs';
 import * as net from 'net';
-
-import fs from 'node:fs';
-import process from 'node:process';
+import * as  process from 'process';
 
 const server: net.Server = net.createServer((socket: net.socket) => {
     // socket.write('HTTP/1.1 200 OK\r\n\r\n');
