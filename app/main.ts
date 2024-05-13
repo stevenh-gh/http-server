634d05febb5102f7b8a3a4b586871547519425e4
steven huynh <steven.h@live.com>
2024-05-13 02:33:48 -0700

    run tests

--- app/main.ts
+++ app/main.ts
@@ -2,25 +2,34 @@ import fs from 'node:fs';
 import net from 'node:net';
 import process from 'node:process';
 
-interface Request {
+interface RequestType {
     getMethod(): string;
     getPath(): string;
     getPathContents(): string[];
     getAcceptEncoding(): string | undefined;
+    getUserAgent(): string | undefined;
+    getContentLength(): number,
+    getContent(): string[]
 }
 
-class Request implements Request {
+class Request implements RequestType {
     private request: string[];
     private method: string;
     private path: string;
     private acceptEncoding: string | undefined;
+    private userAgent: string | undefined;
+    private contentLength: number
+    private content: string[];
 
     public constructor(buffer: net.Buffer | string) {
         this.request = buffer.toString().split(' ').map((str: string) => str.split('\r\n')).flat().filter((str: string) => str.length);
         this.method = this.processMethod(this.request);
         this.path = this.processPath(this.request);
         this.acceptEncoding = this.processAcceptEncoding(this.request);
+        this.userAgent = this.processUserAgent(this.request);
+        this.processContent(this.request);
     }
+
 
     public getMethod(): string {
         return this.method;
@@ -38,7 +47,19 @@ class Request implements Request {
         return this.acceptEncoding;
     }
 
-    private processMethod(request: string[]): string {
+    public getUserAgent(): string | undefined {
+        return this.userAgent;
+    }
+
+    public getContentLength(): number {
+        return this.contentLength;
+    }
+
+    public getContent(): string[] {
+        return this.content;
+    }
+
+    public processMethod(request: string[]): string {
         return request[0].toLowerCase();
     }
 
@@ -47,15 +68,32 @@ class Request implements Request {
     }
 
     private processAcceptEncoding(request: string[]): string | undefined {
-        let encodingExist: string | undefined = request.find((e: string) => { e.toLowerCase() === 'accept-encoding:' })
         let encodingIndex: number = request.findIndex((e: string) => e.toLowerCase() === 'accept-encoding:') + 1;
-        let encoding = request.at(encodingIndex);
-        if (encoding !== 'invalid-encoding') {
-            return encoding;
+        if (encodingIndex !== -1) {
+            let encoding = request.at(encodingIndex);
+            if (encoding !== 'invalid-encoding') {
+                return encoding;
+            }
         }
         return undefined;
     }
 
+    private processUserAgent(request: string[]): string | undefined {
+        let agentIndex: number = request.findIndex((e: string) => e.toLowerCase() === 'user-agent:') + 1;
+        if (agentIndex !== -1) {
+            let agent = request.at(agentIndex);
+            return agent;
+        }
+        return undefined;
+    }
+
+    private processContent(request: string[]): void {
+        let contentLengthIndex: number = request.findIndex((e: string) => e.toLowerCase() === 'content-length:');
+        if (contentLengthIndex !== -1) {
+            this.contentLength = request.at(contentLengthIndex + 1);
+            this.content = request.slice(contentLengthIndex + 2)
+        }
+    }
 }
 
 const server: net.Server = net.createServer((socket: net.socket) => {
@@ -67,11 +105,11 @@ const server: net.Server = net.createServer((socket: net.socket) => {
         // let method: string = request[0].toLowerCase();
         // let path: string = request[1];
         const request: Request = new Request(buffer);
+        let pathContents: string[] = request.getPathContents();
         if (request.getMethod() === 'get') {
             if (request.getPath() === '/') {
                 socket.write('HTTP/1.1 200 OK\r\n\r\n');
             } else {
-                let pathContents: string[] = request.getPathContents();
                 if (pathContents[0] === 'echo') {
                     let encoding = request.getAcceptEncoding();
                     if (encoding) {
@@ -80,8 +118,8 @@ const server: net.Server = net.createServer((socket: net.socket) => {
                     socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${pathContents[1].length}\r\n\r\n${pathContents[1]}`);
 
                 } else if (pathContents[0] === 'user-agent') {
-                    let userAgent: string[] = request.at(-1).trim();
-                    socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`);
+                    let userAgent: string | undefined = request.getUserAgent();
+                    socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent!.length}\r\n\r\n${userAgent}`);
                 } else if (pathContents[0] === 'files') {
                     let directory: string = process.argv[3];
                     let fileName: string = pathContents[1]
@@ -96,13 +134,11 @@ const server: net.Server = net.createServer((socket: net.socket) => {
                 }
             }
         }
-        if (method === 'post') {
-            let pathContents: string[] = path.split('/');
-            pathContents.shift();
+        if (request.getMethod() === 'post') {
             if (pathContents[0] === 'files') {
                 let directory: string = process.argv[3];
                 let fileName: string = pathContents[1]
-                fs.writeFile(directory + fileName, request.slice(7).join(' '), (err: Error) => {
+                fs.writeFile(directory + fileName, request.getContent().slice(7).join(' '), (err: Error) => {
                     if (err) {
                         socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
                     } else {
