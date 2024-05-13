7ff5f2c00fba16b85b2af12a0a77c012f372b016
steven huynh <steven.h@live.com>
2024-05-12 17:11:15 -0700

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -10,4 +10,5 @@ console.log("Logs from your program will appear here!");
 // Uncomment this to pass the first stage
 server.listen(4221, 'localhost', () => {
     console.log('Server is running on port 4221');
+    return 'HTTP/1.1 200 OK\r\n\r\n';
 });
