097fc12e818ae838f6ed89c3e0c22da25d8b652a
steven huynh <steven.h@live.com>
2024-05-12 20:24:11 -0700

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -2,12 +2,12 @@ import * as net from 'net';
 
 const server: net.Server = net.createServer((socket: net.socket) => {
     // socket.write('HTTP/1.1 200 OK\r\n\r\n');
-    socket.on('data', (e) => {
-        console.log(e)
-    })
     socket.end();
 });
 
+server.on('data', (e) => {
+    console.log(e)
+})
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
