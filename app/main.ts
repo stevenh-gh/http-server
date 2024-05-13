61f404e1165de5330b372a6365cc40e845274e54
steven huynh <steven.h@live.com>
2024-05-12 17:34:10 -0700

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,7 @@
 import * as net from 'net';
 
-const server = net.createServer((socket) => {
+const server: net.Server = net.createServer((socket: net.socket) => {
+    socket.write('HTTP/1.1 200 OK \r\n\r\n');
     socket.end();
 });
 
@@ -10,5 +11,4 @@ console.log("Logs from your program will appear here!");
 // Uncomment this to pass the first stage
 server.listen(4221, 'localhost', () => {
     console.log('Server is running on port 4221');
-    return new Response('HTTP/1.1 200 OK \r\n\r\n');
 });
