d864e3b50cadb81d29652e8891878934e2dff12f
steven huynh <steven.h@live.com>
2024-05-19 20:19:26 -0700

    run tests

--- app/main.ts
+++ app/main.ts
@@ -20,7 +20,7 @@ const server: net.Server = net.createServer((socket: net.socket) => {
                     console.log(encoding)
                     if (encoding) {
                         let gzip = zlib.gzipSync(pathContents[1]);
-                        let compress = gzip.toString('hex');
+                        console.log(typeof gzip);
                         // console.log(`HTTP/1.1 200 OK\r\nContent-Encoding: ${encoding}\r\nContent-Type: text/plain\r\nContent-Length: ${compress.length}\r\n\r\n\r\n${compress}`);
                         socket.write(`HTTP/1.1 200 OK\r\nContent-Encoding: ${encoding}\r\nContent-Type: text/plain\r\nContent-Length: ${compress.length}\r\n\r\n${compress}`);
                     }
